a41e49814d822518726771702f748f981b30c797
StarPandaBeg <kshmurakov@mail.ru>
2024-05-04 14:07:44 +0300

    Feature: Add contest view page
    Add align propery to block macro
    Add chip macro

--- blueprints/contest.py
+++ blueprints/contest.py
@@ -6,3 +6,8 @@ bp = Blueprint("contest", __name__, url_prefix="/contests")
 @bp.route("/")
 def index():
     return render_template("contest-list.jinja")
+
+
+@bp.route("/<id>")
+def view(id):
+    return render_template("contest-view.jinja", id=id)
