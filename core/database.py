c84c8ae2341c583f80e2d0580e368a24fa1db5d6
StarPandaBeg <kshmurakov@mail.ru>
2024-05-06 15:57:21 +0300

    Feature: Enable SQLite foreign keys support

--- core/database.py
+++ core/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine, event
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
+from sqlite3 import Connection as SQLite3Connection
 
 from bootstrap.config import app_config
 
@@ -14,3 +15,11 @@ Base.query = db_session.query_property()
 def init_db():
     import core.models
     Base.metadata.create_all(bind=engine)
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
