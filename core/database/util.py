ce9f4a4682e3b07c15fdbf4d90827c59ff7608c5
StarPandaBeg <kshmurakov@mail.ru>
2024-05-06 20:42:46 +0300

    Feature: Override sqlite functions to support case-insensitive search
    Provide custom implementation for NOCASE, LOWER, UPPER

--- core/database/util.py
+++ core/database/util.py
@@ -9,3 +9,26 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
         cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
 
+        dbapi_connection.create_collation("NOCASE", ignore_case_collation)
+        dbapi_connection.create_function("LOWER", 1, sqlite_lower)
+        dbapi_connection.create_function("UPPER", 1, sqlite_upper)
+
+
+def sqlite_lower(value_):
+    # That's required for case-insensitive search
+    return value_.lower()
+
+
+def sqlite_upper(value_):
+    # That's required for case-insensitive search
+    return value_.upper()
+
+
+def ignore_case_collation(value1_, value2_):
+    # That's required for case-insensitive search
+    if value1_.lower() == value2_.lower():
+        return 0
+    elif value1_.lower() < value2_.lower():
+        return -1
+    else:
+        return 1
